9b7adcfca47f13be1e7518419d0b6a51e15c7af5
kylekuminkoski <78025486+kylekuminkoski@users.noreply.github.com>
2025-01-28 14:55:53 -0500

    commit

--- Sandbox.ps1
+++ Sandbox.ps1
@@ -1,1 +1,11 @@
+[CmdletBinding()]
+
+$VerbosePreference = "Continue"
+
+if ( -Not (Test-Path C:\Temp) ) {
+    New-Item -Path "C:\" -Name "Temp" -ItemType Directory
+}
+
+Invoke-WebRequest -Uri "https://download.microsoft.com/download/1/f/0/1f0e9569-3350-4329-b443-822976f29284/ALTools.exe" -OutFile "C:\Windows\Temp\ALTools.exe"
+
 Export-Csv -Path C:\Temp\test.csv
